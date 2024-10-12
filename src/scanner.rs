e6756dc0680fbcf1089a98e2c421f9e65b01f17e
Nifemi Bosun <144700801+nifemibosun@users.noreply.github.com>
2024-10-12 23:34:18 +0100

    Update scanner.rs

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::lox::Lox;
 use crate::token::{ Token, TokenType };
 
@@ -18,6 +20,26 @@ impl Scanner {
             current: 0,
             line: 1,
         }
+    }
+
+    fn keywords() -> HashMap<&'static str, TokenType> {
+        let mut keywords = HashMap::new();
+        keywords.insert("and", TokenType::AND);
+        keywords.insert("class", TokenType::CLASS);
+        keywords.insert("else", TokenType::ELSE);
+        keywords.insert("false", TokenType::FALSE);
+        keywords.insert("for", TokenType::FOR);
+        keywords.insert("func", TokenType::FUNC);
+        keywords.insert("if", TokenType::IF);
+        keywords.insert("nil", TokenType::NIL);
+        keywords.insert("or", TokenType::OR);
+        keywords.insert("print", TokenType::PRINT);
+        keywords.insert("return", TokenType::RETURN);
+        keywords.insert("super", TokenType::SUPER);
+        keywords.insert("self", TokenType::SELF);
+        keywords.insert("true", TokenType::TRUE);
+        keywords.insert("let", TokenType::LET);
+        keywords.insert("while", TokenType::WHILE);
     }
 
     pub fn scan_tokens(&mut self) -> Vec<Token> {
@@ -65,7 +87,7 @@ impl Scanner {
                 } else {
                     self.add_token(TokenType::LESS);
                 }
-              }
+            }
             '>' => {
                 if self.match_next('=') {
                     self.add_token(TokenType::GREATEREQUAL);
@@ -86,8 +108,48 @@ impl Scanner {
             '\n' => {
                 self.line += 1
             }
-            _ => Lox::error(self.line, "Unexpected character")
+            '"' => self.string(),
+            'o' => {
+                if self.match_next('r') {
+                    self.add_token(TokenType::OR);
+                }
+            }
+            _ => {
+                if Scanner::is_digit(c) {
+                    self.number()
+                } else if self.is_alpha(c) {
+                    self.identifier()
+                }else {
+                    Lox::error(self.line, "Unexpected character")
+                }
+            }
         }
+    }
+
+    fn identifier() {
+        while is_alpha_numeric(self.peek()) {
+            self.advance();
+        }
+        let text = &self.source[self.start..self.current];
+        let token_type = self.keywords.get(text).unwrap_or(&TokenType::Identifier);
+        add_token(token_type.clone());
+    }
+
+    fn number(&self) {
+        while Scanner::is_digit(self.peek()) {
+            self.advance();
+        }
+
+        if self.peek() == '.' && Scanner::is_digit(self.peek_next()) {
+            self.advance();
+
+            while Scanner::is_digit(self.peek()) {
+                self.advance();
+            }
+        }
+
+        self.add_token(TokenType::Number, 
+    self.source[self.start..self.current].parse::<f64>().ok());
     }
 
     fn is_at_end(&self) -> bool {
@@ -130,5 +192,45 @@ impl Scanner {
             return '\0';
         }
         return self.source[self.current..].chars().next().unwrap();
+    }
+
+    fn peek_next(&self) -> char {
+        if self.current + 1 >= self.source.len() {
+            return '\0';
+        }
+
+        return self.source[self.current + 1..].chars().next().unwrap_or('\0');
+    }
+
+    fn is_alpha(c: char) -> bool {
+        (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_'
+    }
+
+    fn is_alpha_numeric(c: char) -> bool {
+        Scanner::is_alpha(c) || Scanner::is_digit(c)
+    }
+
+    fn is_digit(c: char) -> bool {
+        c >= '0' && c <= '9'
+    }
+
+    fn string(&mut self) {
+        while self.peek() != '"' && !self.is_at_end() {
+            if self.peek() == '\n' {
+                self.line += 1;
+            }
+            self.advance();
+        }
+
+        if self.is_at_end() {
+            Lox::error(self.line, "Unterminated string.");
+            return;
+        }
+
+        self.advance();
+
+        let value = self.source[(self.start + 1)..(self.current - 1)].to_string();
+
+        self.add_token_literal(TokenType::STRING, Some(value));
     }
 }
